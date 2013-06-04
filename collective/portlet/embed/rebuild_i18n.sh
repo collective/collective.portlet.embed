09d35afa88373157698d9ed4658594c25fa413c9
Luca Fabbri <luca@keul.it>
2013-06-04 18:24:43 +0200

    italian translation

--- collective/portlet/embed/rebuild_i18n.sh
+++ collective/portlet/embed/rebuild_i18n.sh
@@ -4,6 +4,7 @@ I18NDOMAIN=$PRODUCTNAME
 
 # Synchronise the .pot with the templates.
 i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --create ${I18NDOMAIN} .
+i18ndude merge --pot locales/${I18NDOMAIN}.pot --merge locales/${I18NDOMAIN}-manual.pot
 
 # Synchronise the resulting .pot with the .po files
 i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/*/LC_MESSAGES/${PRODUCTNAME}.po
